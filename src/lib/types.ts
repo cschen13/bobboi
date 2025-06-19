8b73a00ecf103a55e4d1230173417662370523a9
Chris Chen <cschen13@gmail.com>
2025-06-19 16:22:14 -0400

    Complete task 2.1: implementing the core data structures

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,1 +1,43 @@
- 
+/**
+ * Core game data structures for Bobboi
+ */
+
+/**
+ * Represents a playing card
+ */
+export interface Card {
+  suit: string;
+  rank: string;
+  value: number;
+}
+
+/**
+ * Represents a player in the game
+ */
+export interface Player {
+  id: string;
+  name: string;
+  card: Card | null;
+}
+
+/**
+ * Represents the game state
+ */
+export interface Game {
+  id: string;
+  players: Player[];
+  deck: Card[];
+  round: number;
+  turn: number;
+  gameState: GameState;
+}
+
+/**
+ * Enum for the different states of the game
+ */
+export enum GameState {
+  WAITING_FOR_PLAYERS = 'WAITING_FOR_PLAYERS',
+  PLAYING = 'PLAYING',
+  ROUND_ENDED = 'ROUND_ENDED',
+  GAME_ENDED = 'GAME_ENDED'
+} 
