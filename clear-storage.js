b489fb0107310e7dee2e8a01d5afe643439ae911
Chris Chen <cschen13@gmail.com>
2025-07-19 23:00:47 -0400

    Add debug file for guidance

---
+++ clear-storage.js
@@ -0,0 +1,5 @@
+// Run this in browser console to clear localStorage for testing
+localStorage.removeItem('bobboi_game_id');
+localStorage.removeItem('bobboi_player_id');
+localStorage.removeItem('bobboi_player_name');
+console.log('Bobboi game session cleared from localStorage');
